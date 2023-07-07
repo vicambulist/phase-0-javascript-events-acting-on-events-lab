02f6d58e810cc02c296fbab31c1ccd0ca2b0eb33
Bret Barden <bret.barden@gmail.com>
2023-07-07 12:02:27 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+// Your code here //
+
+
+// Test 1 //
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+
